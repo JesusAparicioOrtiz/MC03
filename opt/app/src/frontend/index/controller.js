9a9158acbd1b4c047cccbdec5806507589dd0ca8
Jesus Aparicio <72458998+JesusAparicioOrtiz@users.noreply.github.com>
2021-04-23 21:31:21 +0200

    Initial Commit

---
+++ opt/app/src/frontend/index/controller.js
@@ -0,0 +1,45 @@
+/*!
+governify-render 1.0.0, built on: 2018-05-09
+Copyright (C) 2018 ISA group
+http://www.isa.us.es/
+https://github.com/isa-group/governify-render#readme
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+$scope.displayError = "none"
+
+/*! Añade una tarea a tareas
+ */
+$scope.addTask = function() {
+    if (angular.isDefined($scope.title) && $scope.title != "") {
+        // Add new task.
+        $scope.model.tasks.push({ title: $scope.title, description: $scope.description });
+
+        // Clear fields.
+        $scope.displayError = "none"
+        $scope.title = "";
+        $scope.description = "";
+        $scope.errorForm = ""
+    }else{
+        $scope.displayError = "block"
+        $scope.errorForm = "Title must be filled"
+    }
+}
+
+/*! Borra una tarea de las tareas
+ *  @param idx entero que representa el índice de la tarea en el array de tareas
+ */
+$scope.deleteTask = function(idx) {
+    $scope.model.tasks.splice(idx,1);
+}
